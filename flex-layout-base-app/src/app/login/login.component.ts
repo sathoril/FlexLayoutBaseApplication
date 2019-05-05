00506ca925b464235d6cbe283e30a90536bf9186
Sathoril <bruno.feabreu@gmail.com>
2019-05-05 18:47:28 -0300

    Some changes was made

--- flex-layout-base-app/src/app/login/login.component.ts
+++ flex-layout-base-app/src/app/login/login.component.ts
@@ -1,37 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Router } from '@angular/router';
-import { NavigationService } from 'src/shared/services/navigation.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from "@angular/cdk/layout";
+import { Router } from "@angular/router";
+import { NavigationService } from "src/shared/services/navigation.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
-
 export class LoginComponent implements OnInit {
-
   // Allows for the password to be revealed or not
   hide = true;
 
   // It changes to true if the device is a Mobile device and sets false if the device is a Desktop
   isMobile: Boolean;
 
-  constructor(private breakpointObserver: BreakpointObserver, private router: Router, private navigationService: NavigationService) { 
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private navigationService: NavigationService
+  ) {
     this.breakpointObserver
-    .observe([ Breakpoints.Small, Breakpoints.HandsetPortrait ])
-    .subscribe((state: BreakpointState) => {
-      this.isMobile = state.matches;
-    });
+      .observe([Breakpoints.Small, Breakpoints.HandsetPortrait])
+      .subscribe((state: BreakpointState) => {
+        this.isMobile = state.matches;
+      });
 
     this.navigationService.hide();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   loginUser() {
-    this.router.navigate(['/home']);
+    this.router.navigate(["/home"]);
   }
 }
